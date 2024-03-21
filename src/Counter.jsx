8bb52fc74889563fb06d36b64cfae69f8954fb27
unknown <jithinbinoy2000@gmail.com>
2024-03-21 09:20:34 +0530

    Featrue Added : user can also enter the value

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,23 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { decrement, increment, reset } from './redux/countslice'
+import { decrement, increment, incrementByAmount, reset } from './redux/countslice'
 
 export default function Counter() {
  const dispatch=useDispatch()
  const count = useSelector((state)=>state.counterReduce.count)
+ //state to store user input value
+ const [amount,setamount]=useState("")
+ //function for validation and dispatch
+ const handleIncrement=()=>{
+  if(amount===""){
+    alert("Please Fill The Form Completely")
+  }else{
+    dispatch(incrementByAmount(Number(amount)))
+   
+  }
+ }
+ const borderColor = count > 0 ? "green" : count < 0 ? "red" : "white";
   return (
     
    <div>
        <div className='d-flex align-items-center justify-content-center mt-5'style={{width:"100%"}}>
-            <div className='counterframe d-flex align-items-center justify-content-center' >
-                <div className='result'>{count}</div>
+            <div className='counterframe d-flex align-items-center justify-content-center' style={{ border: `2px solid ${borderColor}` }}>
+                <div className='result' style={{color:`${borderColor}`}}>{count}</div>
              </div>
        </div>
          <div className='mt-5 d-flex align-items-center justify-content-center'>
             <div className="btn btn-warning me-2" onClick={()=>dispatch(decrement())}><i className="fa-solid fa-minus  "></i></div>
-            <div className="btn btn-danger me-2 " onClick={()=>dispatch(reset())}>Reset</div>
+            <div className="btn btn-danger me-2 " onClick={()=>{dispatch(reset());setamount("");}}>Reset</div>
             {/* increment */}
-            <div className="btn btn-success" onClick={()=>dispatch(increment())}><i className="fa-solid fa-plus "></i></div>
+            <div className="btn btn-success" onClick={()=>{dispatch(increment());}}><i className="fa-solid fa-plus "></i></div>
+         </div>
+         <div className='d-flex justify-content-center mt-3'>
+           <input type="text" className='form-control me-1'placeholder='Increment Amount'  style={{width:'180px'}}
+            onChange={e=>setamount(e.target.value)} value={amount}/>
+
+           <button className='btn btn-primary' onClick={handleIncrement}><i className='fa-solid fa-plus'></i></button>
          </div>
    </div>
   )
--- src/redux/countslice.js
+++ src/redux/countslice.js
@@ -14,13 +14,16 @@ const countSlice =createSlice({
         },
         reset:(state)=>{
             state.count=0
-        }
+        },
+        incrementByAmount:(state,action)=>{
+               state.count+=action.payload
+        }   
     }
 
 })
 //export actions and store reducer seperately
 //export action 
 //Action creators for the types of actions that are handled by the slice reducer.
-export  const {increment,decrement,reset}=countSlice.actions
+export  const {increment,decrement,reset,incrementByAmount}=countSlice.actions
 //export slice reducers
 export default countSlice.reducer
